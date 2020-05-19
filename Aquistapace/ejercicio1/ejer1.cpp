e7d7dd4bc7275a00b526219704d654a8583d5b32
Tomas-Aquistapace <aquista35@gmail.com>
2020-05-19 09:46:12 -0300

    Inicio del primer ejercicio

---
+++ Aquistapace/ejercicio1/ejer1.cpp
@@ -0,0 +1,14 @@
+#include "ejer1.h"
+
+namespace parcial
+{
+	void MostrarCreditos()
+	{
+		
+	}
+
+	void Diferencia(float num1, float num2)
+	{
+		
+	}
+}
---
+++ Aquistapace/ejercicio1/ejer1.h
@@ -0,0 +1,10 @@
+#ifndef EJER1
+#define EJER1
+
+namespace parcial
+{
+	void MostrarCreditos();
+	void Diferencia(float num1, float num2);
+}
+
+#endif //EJERCICIO1
